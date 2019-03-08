3a581253f3d172eed25a9a8c75c57cd80165f65c
LukeMcCann <developer-lukemccann@outlook.com>
2019-03-08 15:59:30 +0000

    initial commit

---
+++ src/webservice/Calculator.java
@@ -0,0 +1,24 @@
+package webservice;
+
+public class Calculator {
+
+	public double add(int x, int y)
+	{
+		return x + y;
+	}
+	
+	public double minus(int x, int y)
+	{
+		return x - y;
+	}
+	
+	public double multiply(int x, int y)
+	{
+		return x * y;
+	}
+	
+	public double divide(int x, int y)
+	{
+		return x / y;
+	}
+}
